f48d69e6621ae74becc382793fca4389bd3130c0
手瓜一十雪 <nanaeonn@outlook.com>
2024-10-19 11:06:27 +0800

    fix: thank elysia

--- src/.vitepress/theme/index.js
+++ src/.vitepress/theme/index.js
@@ -1,5 +1,10 @@
 // .vitepress/theme/index.js
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
+import Layout from './Layout.vue'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  // 使用注入插槽的包装组件覆盖 Layout
+  Layout: Layout
+}
