8f7a6130969e08e787acd6e305903394d60bfe7b
手瓜一十雪 <nanaeonn@outlook.com>
2024-11-14 10:05:32 +0800

    fix

--- src/.vitepress/config/shared.ts
+++ src/.vitepress/config/shared.ts
@@ -37,14 +37,14 @@ export const shared = defineConfig({
         href: '/assets/logo.png'
       }
     ],
-    ['link', {rel: 'icon', type: 'image/png', sizes: '16x16', href: 'https://avatars.githubusercontent.com/u/165024194'}],
-    ['link', {rel: 'apple-touch-icon', sizes: '180x180', href: 'https://avatars.githubusercontent.com/u/165024194'}],
-    ['link', {rel: 'mask-icon', href: 'https://avatars.githubusercontent.com/u/165024194', color: '#5bbad5'}],
+    ['link', {rel: 'icon', type: 'image/png', sizes: '16x16', href: '/assets/logo.png'}],
+    ['link', {rel: 'apple-touch-icon', sizes: '180x180', href: '/assets/logo.png'}],
+    ['link', {rel: 'mask-icon', href: '/assets/logo.png', color: '#5bbad5'}],
     ['meta', { name: 'theme-color', content: '#5f67ee' }],
   ],
 
   themeConfig: {
-    logo: { src: 'https://avatars.githubusercontent.com/u/165024194', width: 24, height: 24 },
+    logo: { src: '/assets/logo.png', width: 24, height: 24 },
     //开启本地搜索
     search: {
       provider: 'local'
