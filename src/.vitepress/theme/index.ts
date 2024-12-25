ae7ed950c9f70de9205d3002274d523eb18303e7
手瓜一十雪 <nanaeonn@outlook.com>
2024-12-25 14:41:22 +0800

    fix

--- src/.vitepress/theme/index.ts
+++ src/.vitepress/theme/index.ts
@@ -7,24 +7,28 @@ import {
   Options
 } from '@nolebase/vitepress-plugin-git-changelog/client';
 import { NolebasePagePropertiesPlugin } from '@nolebase/vitepress-plugin-page-properties';
-import { 
-  NolebaseEnhancedReadabilitiesMenu, 
-  NolebaseEnhancedReadabilitiesScreenMenu, 
+import {
+  NolebaseEnhancedReadabilitiesMenu,
+  NolebaseEnhancedReadabilitiesScreenMenu,
 } from '@nolebase/vitepress-plugin-enhanced-readabilities/client';
 import TwoslashFloatingVue from '@shikijs/vitepress-twoslash/client';
 import { NolebaseEnhancedReadabilitiesPlugin } from '@nolebase/vitepress-plugin-enhanced-readabilities/client';
+import codeblocksFold from 'vitepress-plugin-codeblocks-fold';
+import { useData, useRoute } from 'vitepress';
 
 import { InjectionKey } from '@nolebase/vitepress-plugin-git-changelog/client';
 import "vitepress-markdown-timeline/dist/theme/index.css";
 import '@nolebase/vitepress-plugin-git-changelog/client/style.css'
 import '@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css';
+import 'vitepress-plugin-codeblocks-fold/style/index.css';
+
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
-      'nav-bar-content-after': () => h(NolebaseEnhancedReadabilitiesMenu), 
-      'nav-screen-content-after': () => h(NolebaseEnhancedReadabilitiesScreenMenu), 
+      'nav-bar-content-after': () => h(NolebaseEnhancedReadabilitiesMenu),
+      'nav-screen-content-after': () => h(NolebaseEnhancedReadabilitiesScreenMenu),
     })
   },
   enhanceApp({ app }) {
@@ -34,46 +38,51 @@ export default {
         defaultToggle: true,
       }
     } as Options),
-    app.use(TwoslashFloatingVue as unknown as Plugin),
-    app.use(NolebaseGitChangelogPlugin),
-    app.provide(InjectionKey, {
-      hideChangelogNoChangesText: true,
-      commitsRelativeTime: true,
-      displayAuthorsInsideCommitLine: true,
-      hideContributorsHeader: true,
-      hideChangelogHeader: true
-    }),
-    app.use(NolebasePagePropertiesPlugin<{
-      progress: number
-    }>() as Plugin,{
-      properties: {
-        'zh-CN': [
-          {
-            key: 'wordCount',
-            type: 'dynamic',
-            title: '字数',
-            options: {
-              type: 'wordsCount',
+      app.use(TwoslashFloatingVue as unknown as Plugin),
+      app.use(NolebaseGitChangelogPlugin),
+      app.provide(InjectionKey, {
+        hideChangelogNoChangesText: true,
+        commitsRelativeTime: true,
+        displayAuthorsInsideCommitLine: true,
+        hideContributorsHeader: true,
+        hideChangelogHeader: true
+      }),
+      app.use(NolebasePagePropertiesPlugin<{
+        progress: number
+      }>() as Plugin, {
+        properties: {
+          'zh-CN': [
+            {
+              key: 'wordCount',
+              type: 'dynamic',
+              title: '字数',
+              options: {
+                type: 'wordsCount',
+              },
             },
-          },
-          {
-            key: 'readingTime',
-            type: 'dynamic',
-            title: '阅读时间',
-            options: {
-              type: 'readingTime',
+            {
+              key: 'readingTime',
+              type: 'dynamic',
+              title: '阅读时间',
+              options: {
+                type: 'readingTime',
+                dateFnsLocaleName: 'zhCN',
+              },
+            },
+            {
+              key: 'updatedAt',
+              type: 'datetime',
+              title: '更新时间',
+              formatAsFrom: true,
               dateFnsLocaleName: 'zhCN',
             },
-          },
-          {
-            key: 'updatedAt',
-            type: 'datetime',
-            title: '更新时间',
-            formatAsFrom: true,
-            dateFnsLocaleName: 'zhCN',
-          },
-        ],
-      },
-    })
+          ],
+        },
+      })
   },
+  setup() {
+    const route = useRoute();
+    const { frontmatter } = useData();
+    codeblocksFold({ route, frontmatter }, true, 400);
+  }
 }
