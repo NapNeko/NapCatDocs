a3c3af6ea63bbc2ac4f10f16a0fc07860d754431
手瓜一十雪 <nanaeonn@outlook.com>
2025-04-29 22:11:35 +0800

    fix

--- src/.vitepress/data/footerData.ts
+++ src/.vitepress/data/footerData.ts
@@ -23,7 +23,10 @@ export const Footer_Data: FooterData = {
             title: '社区',
             icon: 'fa-solid fa-expand',
             links: [
-                { name: '官方 企鹅 社区', href: 'https://qm.qq.com/q/I6LU87a0Yq' },
+                { name: '官方 企鹅 4号社区', href: 'https://qm.qq.com/q/CMmPbGw0jA' },
+                { name: '官方 企鹅 3号社区', href: 'https://qm.qq.com/q/8zJMLjqy2Y' },
+                { name: '官方 企鹅 2号社区', href: 'https://qm.qq.com/q/HaRcfrHpUk' },
+                { name: '官方 企鹅 1号社区', href: 'https://qm.qq.com/q/I6LU87a0Yq' },
                 { name: '官方 Telegram 社区', href: 'https://t.me/MelodicMoonlight' },
             ]
         }
